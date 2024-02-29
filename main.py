92b75ae8ecd1e1315a93ba06f3488f50e52d6191
Arturo Alva <124171994+ArtuIVO@users.noreply.github.com>
2024-02-29 09:51:14 -0600

    Codigo finalizado

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from polinomio import Polinomio
 polinomio_a = Polinomio()
 polinomio_b = Polinomio()
+
 cons = 0
 
 print("Bienvenido al programa de polinomios hecho por Arturo Alva 1525123")
@@ -16,14 +17,50 @@ while True:
     datop = int(input())
 
     if datop == 1:
-        print("De que grado sera su polinomio ?")
-        a = int(input("Ingrese el gardo: "))
-
-
+        polinomio = input("Ingrese el nombre del polinomio (a o b): ").strip().lower()
+        coeficiente = float(input("Ingrese el coeficiente del término: "))
+        exponente = int(input("Ingrese el exponente del término: "))
+        if polinomio == "a":
+            polinomio_a.agregar_termino(coeficiente, exponente)
+            print(polinomio_a)
+        elif polinomio == "b":
+            polinomio_b.agregar_termino(coeficiente, exponente)
+            print(polinomio_b)
+        else:
+            print("Polinomio no válido.")
     elif datop == 2:
-        pass
+        print("Que desea hacer, sumar (s) o restar (r)")
+        b = input().lower()
+
+        if b == "s":
+            print("Sumar polinomios:")
+            print("Polinomio A:")
+            polinomio_a.mostrar_polinomio()
+            print("Polinomio B:")
+            polinomio_b.mostrar_polinomio()
+
+            resultado_suma = polinomio_a.sumar_polinomios(polinomio_b)
+            print("Resultado de la suma:")
+            resultado_suma.mostrar_polinomio()
+        elif b == "r":
+
+            print("Restar polinomios:")
+            resultado_resta = polinomio_a.restar_polinomios(polinomio_b)
+            print("Resultado de la resta:")
+            resultado_resta.mostrar_polinomio()
+        else:
+            print("Dato incorrecto")
     elif datop == 3:
-        pass
+        polinomio = input("Ingrese el nombre del polinomio (a o b) que desea evaluar: ").strip().lower()
+        valor_x = float(input("Ingrese el valor de x: "))
+        if polinomio == "a":
+            resultado = polinomio_a.evaluar_polinomio(valor_x)
+            print(f"Resultado de evaluar polinomio 'a': {resultado}")
+        elif polinomio == "b":
+            resultado = polinomio_b.evaluar_polinomio(valor_x)
+            print(f"Resultado de evaluar polinomio 'b': {resultado}")
+        else:
+            print("Polinomio no válido.")
     elif datop == 4:
         print("\n")
         print(" Gracias por usar el programa ")
--- polinomio.py
+++ polinomio.py
@@ -15,6 +15,9 @@ class Polinomio:
                 actual = actual.siguiente
             actual.siguiente = nuevo_termino
 
+        print("Polinomio actualizado:")
+        self.mostrar_polinomio()
+
     def mostrar_polinomio(self):
         actual = self.cabeza
         polinomio_str = ""
@@ -37,3 +40,61 @@ class Polinomio:
             resultado += actual.coeficiente * (x ** actual.exponente)
             actual = actual.siguiente
         return resultado
+
+    def sumar_polinomios(self, otro_polinomio):
+        nuevo_polinomio = Polinomio()
+        actual1 = self.cabeza
+        actual2 = otro_polinomio.cabeza
+
+        while actual1 and actual2:
+            if actual1.exponente == actual2.exponente:
+                nuevo_polinomio.agregar_termino(actual1.coeficiente + actual2.coeficiente, actual1.exponente)
+                actual1 = actual1.siguiente
+                actual2 = actual2.siguiente
+            elif actual1.exponente > actual2.exponente:
+                nuevo_polinomio.agregar_termino(actual1.coeficiente, actual1.exponente)
+                actual1 = actual1.siguiente
+            else:
+                nuevo_polinomio.agregar_termino(actual2.coeficiente, actual2.exponente)
+                actual2 = actual2.siguiente
+
+        while actual1:
+            nuevo_polinomio.agregar_termino(actual1.coeficiente, actual1.exponente)
+            actual1 = actual1.siguiente
+
+        while actual2:
+            nuevo_polinomio.agregar_termino(actual2.coeficiente, actual2.exponente)
+            actual2 = actual2.siguiente
+
+        print("Polinomio suma:")
+        nuevo_polinomio.mostrar_polinomio()
+        return nuevo_polinomio
+
+    def restar_polinomios(self, otro_polinomio):
+        nuevo_polinomio = Polinomio()
+        actual1 = self.cabeza
+        actual2 = otro_polinomio.cabeza
+
+        while actual1 and actual2:
+            if actual1.exponente == actual2.exponente:
+                nuevo_polinomio.agregar_termino(actual1.coeficiente - actual2.coeficiente, actual1.exponente)
+                actual1 = actual1.siguiente
+                actual2 = actual2.siguiente
+            elif actual1.exponente > actual2.exponente:
+                nuevo_polinomio.agregar_termino(actual1.coeficiente, actual1.exponente)
+                actual1 = actual1.siguiente
+            else:
+                nuevo_polinomio.agregar_termino(-actual2.coeficiente, actual2.exponente)
+                actual2 = actual2.siguiente
+
+        while actual1:
+            nuevo_polinomio.agregar_termino(actual1.coeficiente, actual1.exponente)
+            actual1 = actual1.siguiente
+
+        while actual2:
+            nuevo_polinomio.agregar_termino(-actual2.coeficiente, actual2.exponente)
+            actual2 = actual2.siguiente
+
+        print("Polinomio resta:")
+        nuevo_polinomio.mostrar_polinomio()
+        return nuevo_polinomio
