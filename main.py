53ec69b45765ba927917c2716d3f1102f983708b
Arturo Alva <124171994+ArtuIVO@users.noreply.github.com>
2024-02-29 09:00:08 -0600

    Se creo el menú para el uso del codigo

---
+++ main.py
@@ -0,0 +1,32 @@
+from list import Lista
+
+polinomios = Lista()
+
+
+print("Bienvenido al programa de polinomios hecho por Arturo Alva 1525123")
+while True:
+    print("\n")
+    print("Que desea hacer ?")
+    print("1. Ingresar componentes a un polinomio ")
+    print("2. Adición y sustraccion de un polinomio ")
+    print("3. Evaluar polinomio ")
+    print("4. Salir ")
+    print("\n")
+
+    datop = int(input())
+
+    if datop == 1:
+        pass
+    elif datop == 2:
+        pass
+    elif datop == 3:
+        pass
+    elif datop == 4:
+        print("\n")
+        print(" Gracias por usar el programa ")
+        break
+    else:
+        print(" Número del menú no existente, vuelva a intentarlo ")
+        print("\n")
+
+
